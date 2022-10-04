257a8b410f52e1ff00a06456b7d8998f1f895787
dev-damdam <dev.sodam@gmail.com>
2022-10-05 00:35:33 +0900

    add routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Login</router-link> |
+      <router-link to="/home">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
@@ -18,8 +19,6 @@
 }
 
 nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
---
+++ src/router/routes.js
@@ -0,0 +1,23 @@
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+];
+
+export default routes;
