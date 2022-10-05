27c31c4d1516f0200f41fda42d9f0d19d27c667f
dev-damdam <dev.sodam@gmail.com>
2022-10-06 01:41:20 +0900

    remove nav router link

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Login</router-link> |
-      <router-link to="/home">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view />
   </div>
 </template>
