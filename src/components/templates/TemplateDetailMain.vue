5f82a63c9fcff7e30f84921f95d72338629a6b1c
dev-damdam <dev.sodam@gmail.com>
2022-10-07 00:48:49 +0900

    add detail templates

---
+++ src/components/templates/TemplateDetailMain.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tempalte-detail-main"><slot /></div>
+</template>
+<script>
+export default {
+  name: "template-detail-main",
+};
+</script>
+<style lang="scss" scoped>
+.tempalte-detail-main {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/templates/TemplatesDetailHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tempalte-detail-header">
+    <b-icon class="h3" icon="chevron-left" />
+    <span class="id">#{{ id }}</span>
+    <span class="title">{{ title }}</span>
+  </div>
+</template>
+<script>
+export default {
+  name: "template-detail-header",
+  props: {
+    id: {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.tempalte-detail-header {
+  width: 100%;
+
+  border-bottom: 1px solid #dee2e6;
+  padding: 1.5%;
+  text-align: left;
+
+  display: flex;
+  align-items: center;
+
+  .id {
+    color: #afafaf;
+    font-size: 1.3rem;
+    margin-left: 0.8%;
+    margin-right: 0.6%;
+  }
+  .title {
+    font-size: 2rem;
+  }
+}
+</style>
